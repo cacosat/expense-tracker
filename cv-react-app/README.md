2a019208a7b0d24627cdaa5b33e1ddcbd98f8bca
Joaquin Sateler <86966872+cacosat@users.noreply.github.com>
2023-12-16 20:28:21 -0300

    setting up tailwind and testing

--- cv-react-app/README.md
+++ cv-react-app/README.md
@@ -1,8 +1,8 @@
-# React + Vite
+# CV project
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Think about how to structure your application into components. Your application should include:
+1. A section to add general information like name, email and phone number.
+2. A section to add your educational experience (school name, title of study and date of study)
+3. A section to add practical experience (company name, position title, main responsibilities of your jobs, date from and until when you worked for that company)
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Be sure to include an edit and submit button for each section or for the whole CV. The submit button should submit your form and display the value of your input fields in HTML elements. The edit button should add back (display) the input fields, with the previously displayed information as values. In those input fields, you should be able to edit and resubmit the content. You’re going to make heavy use of state and props, so make sure you understood those concepts.
--- cv-react-app/src/App.jsx
+++ cv-react-app/src/App.jsx
@@ -8,28 +8,67 @@ function App() {
 
   return (
     <>
+      <div className="App">
+        <h1 className="text-3xl font-bold underline">
+          Hello, Tailwind!
+        </h1>
+      </div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Dropdown />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
+
+const Dropdown = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value.toLowerCase());
+  };
+
+  const items = ['Uppercase', 'Lowercase', 'Camel Case', 'Kebab Case'];
+
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="relative group">
+        <button 
+          id="dropdown-button" 
+          className="inline-flex justify-center w-full px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500"
+          onClick={toggleDropdown}
+        >
+          <span className="mr-2">Open Dropdown</span>
+          {/* SVG code here */}
+        </button>
+        {isOpen && (
+          <div id="dropdown-menu" className="absolute right-0 mt-2 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 p-1 space-y-1">
+            <input 
+              id="search-input" 
+              className="block w-full px-4 py-2 text-gray-800 border rounded-md  border-gray-300 focus:outline-none" 
+              type="text" 
+              placeholder="Search items" 
+              autocomplete="off"
+              value={searchTerm}
+              onChange={handleSearchChange}
+            />
+            {items
+              .filter((item) => item.toLowerCase().includes(searchTerm))
+              .map((item, index) => (
+                <a key={index} href="#" className="block px-4 py-2 text-gray-700 hover:bg-gray-100 active:bg-blue-100 cursor-pointer rounded-md">
+                  {item}
+                </a>
+              ))
+            }
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default App
