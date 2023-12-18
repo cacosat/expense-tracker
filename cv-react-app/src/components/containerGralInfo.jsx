2289fcf5e0622de514ffa5a61390acbd5c7e1206
Joaquin Sateler <86966872+cacosat@users.noreply.github.com>
2023-12-17 22:27:06 -0300

    Primera sección, info general

--- cv-react-app/src/components/containerGralInfo.jsx
+++ cv-react-app/src/components/containerGralInfo.jsx
@@ -8,14 +8,29 @@ function ContainerGralInfo() {
   // return jsx
   return <>
   <section id="Info" className="text-left">
-    <h1>Título</h1>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit quae minus explicabo aliquid id velit modi tenetur perspiciatis debitis nulla voluptates nihil tempore magnam veritatis, suscipit sit pariatur delectus cupiditate.</p>
+    <div className="flex flex-col px-12 pt-12 py-6 gap-4">
+      <h1 className="text-xl font-bold">NOMBRE</h1>
+      <p><span id="name">Joaquín Sateler</span></p>
+    </div>
+    <div className="flex flex-col px-12 py-6 gap-4">
+      <h1 className="text-xl font-bold">EMAIL</h1>
+      <p><span id="email">ejemplo@gmail.com</span></p>
+    </div>
+    <div className="flex flex-col px-12 py-6 gap-4">
+      <h1 className="text-xl font-bold">TELÉFONO</h1>
+      <p><span id="phone">+56912345678</span></p>
+    </div>
   </section>
-  <section id="Edit" className="text-black">
-    <form action="">
-        <label htmlFor="name" className="text-white">Nombre: </label>
-        <input name="name" type="text" placeholder="Nombre" />
-        <label htmlFor="email">Email</label>
+  <section id="Edit" className="flex justify-center ">
+    <form action="" className="flex gap-8">
+        <div className="flex flex-col text-start max-w-xs">
+          <label htmlFor="name">Nombre: </label>
+          <input name="name" type="text" placeholder="Joaquín Sateler" />
+        </div>
+        <div className="flex flex-col text-start max-w-xs">
+          <label htmlFor="email">Correo</label>
+          <input name="email" type="text" placeholder="ejemplo@gmail.com" />
+        </div>
     </form>
   </section>
   </>
