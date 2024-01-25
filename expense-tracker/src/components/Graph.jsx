95a1f4e70b414f3f11ca19cdb5f336fc407a0b5f
Joaquin Sateler <86966872+cacosat@users.noreply.github.com>
2024-01-24 23:07:12 -0300

    changed line graph to pie chart of total expenses, easier like that

--- expense-tracker/src/components/Graph.jsx
+++ expense-tracker/src/components/Graph.jsx
@@ -31,72 +31,53 @@ export default function Graph(props) {
     fetchExpenses();
   }, []);
 
-  function dataToGraphData(categoriesData, expensesData) {
-    // the format is:
-    // data = [[catgories (first one defines x axis)], 
-    // [N filas, cada una representa datos con la misma ubicación del eje x], ...];
-    // Also graphData should show 10 days x axis fixed (?)
+  function rawDataToGraphData(categoriesData, expensesData) {
+    // the format is each row represent a slice [category, totalAmount],
+    // first row is [sliceLabels, sliceValues], here: ['Categories', 'Total Expenses']
     
+    let graphData = [];
+    graphData.push(['Categories', 'Total Expenses'])
+    // TODO: 
+    // 1. calculate total expenses
+    categoriesData.forEach((category) => {
+      let catName = category.name;
+      let categoryExpenses = expensesData.filter(expense => category.name === expense.category_name);
+      let categoryExpensesTotal = categoryExpenses.reduce((total, expense) => {
+        return total + expense.amount;
+      }, 0);
+      graphData.push([category.name, categoryExpensesTotal]);
+    })
     
-
+    console.log(graphData)
     return graphData;
   }
-  dataToGraphData(categories, expenses);
+  const data = rawDataToGraphData(categories, expenses);
 
-    // data will show last 10 days fixed;
-    const data = [
-        ['Day',/* 'total',*/ 'Alojamiento', 'Comida', 'Movilización', 'Entretenimiento','Otros',],
-        [1, 168990, 7800, 15620, 25800, 8000],
-        [2, 0, 9500, 130000, 15000, 15000],
-        [3, 0, 12000, 10590, 6700, 19852],
-        [4, 60000, 41000, 11000, 27000, 2000],
-        [5, 0, 28000, 8000, 5800, 8000],
-        [6, 0, 9800, 3000, 29000, 3000],
-        [7, 53000, 19700, 104000, 36650, 10568],
-        [8, 0, 10100, 19000, 28000, 20560],
-        [9, 45000, 9900, 36500, 2000, 38520],
-        [10, 37000, 15200, 7700, 10365, 8000],
-        [11, 56000, 5500, 86000, 63000, 85000]
-
-        // With totalValue at index[1]
-        // [1, 226210, 168990, 7800, 15620, 25800, 8000],
-        // [2, 395710, 0, 9500, 130000, 15000, 15000],
-        // [3, 444852, 0, 12000, 10590, 6700, 19852],
-        // [4, 828852, 60000, 41000, 11000, 270000, 2000],
-        // [5, 878652, 0, 28000, 8000, 5800, 8000],
-        // [6, 923452, 0, 9800, 3000, 29000, 3000],
-        // [7, 1147370, 53000, 19700, 104000, 36650, 10568],
-        // [8, 1405030, 0, 10100, 19000, 28000, 200560],
-        // [9, 1536950, 45000, 9900, 36500, 2000, 38520],
-        // [10, 1615215, 37000, 15200, 7700, 10365, 8000],
-        // [11, 1910715, 56000, 5500, 86000, 63000, 85000]
-      ];
+  // const data = [
+  //   ["Task", "Hours per Day"],
+  //   ["Work", 11],
+  //   ["Eat", 2],
+  //   ["Commute", 2],
+  //   ["Watch TV", 2],
+  //   ["Sleep", 7], // CSS-style declaration
+  // ];
     
     const options = {
-        backgroundColor: '#000000',
-        hAxis: {
-            textStyle: { color: '#FFFFFF' }, // Custom color for horizontal axis labels
-            gridlines: { color: 'transparent' },
-            // viewWindow: { // modify depending on width
-            //   min: 0,
-            //   max: 5
-            // },
+        // title: 'titulo',
+        titleTextStyle: {color: 'white'},
+        pieHole: 0.5,
+        backgroundColor: 'transparent',
+        chartArea: {
+          top: '25%',
         },
-        vAxis: {
-            textStyle: { color: '#FFFFFF' }, // Custom color for vertical axis labels
-            gridlines: { color: '#27272A' },
-            viewWindow: {
-                min: 0,
-                max: 200000
-              },
-        },
-        colors: ['white', 'red', 'blue'],
-        curveType: "function",
         legend: { 
             position: "top", 
-            textStyle: {color: '#FFFFFF'} 
+            alignment: "center",
+            textStyle: {color: '#FFFFFF'},
+            maxLines: 2 
         },
-        colors: ['#D32F2F', '#1976D2', '#388E3C', '#FBC02D', '#7B1FA2'],
+        pieSliceBorderColor: '#000000'
+        // colors: ['#D32F2F', '#1976D2', '#388E3C', '#FBC02D', '#7B1FA2'],
     };
 
     return <>
@@ -106,7 +87,7 @@ export default function Graph(props) {
       </div>
       <div className={`flex flex-col xxs:w-[90vw] xs:w-[60vw] sm:w-[70vw] lg:w-[752px] justify-center py-4 border-2 rounded-2xl border-stone-700 overflow-hidden`}>
         <Chart
-            chartType="LineChart"
+            chartType="PieChart"
             height= "350px"
             data={data}
             options={options}
